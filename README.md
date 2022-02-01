fb12e2935239a31eea4cb529c15aa5bb725af42f
Thomas von Rosenberg <thomas@von-rosenberg.eu>
2022-02-01 11:40:18 +0100

    Copy heapify and overall interface from itertools
    See
    https://github.com/rust-itertools/itertools/blob/6c4fc2f8e745fe4780578dfa4feb44ccccffb521/src/kmerge_impl.rs
    for the original code.

--- README.md
+++ README.md
@@ -26,3 +26,10 @@ https://www.apache.org/licenses/LICENSE-2.0 or the MIT license
 https://opensource.org/licenses/MIT, at your
 option. This file may not be copied, modified, or distributed
 except according to those terms.
+
+Portions of the project have been copied from
+https://github.com/rust-itertools/itertools which is copyrighted under the
+Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0 or the
+MIT license https://opensource.org/licenses/MIT, at your option. See
+https://github.com/rust-itertools/itertools/blob/6c4fc2f8e745fe4780578dfa4feb44ccccffb521/src/kmerge_impl.rs
+for the original code.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-use std::cmp::Ordering;
 use std::task::Poll;
 
-use futures::{
-    future::{join_all, JoinAll},
-    ready,
-    stream::StreamFuture,
-    FutureExt, Stream, StreamExt,
-};
+use futures::future::{join_all, JoinAll};
+use futures::{ready, stream::StreamFuture, FutureExt, Stream, StreamExt};
 use pin_project_lite::pin_project;
 
+#[derive(Debug)]
 struct HeadTail<S>
 where
     S: Stream,
@@ -17,27 +13,57 @@ where
     tail: S,
 }
 
-pub trait KMergePredicate<T> {
-    fn kmerge_pred(&mut self, a: &T, b: &T) -> Ordering;
-}
-
-#[derive(Clone, Debug)]
-pub struct KMergeByLt;
-
-impl<T: Ord> KMergePredicate<T> for KMergeByLt {
-    fn kmerge_pred(&mut self, a: &T, b: &T) -> Ordering {
-        a.cmp(b)
+/// Make `data` a heap (min-heap w.r.t the sorting).
+fn heapify<T, S>(data: &mut [T], mut less_than: S)
+where
+    S: FnMut(&T, &T) -> bool,
+{
+    for i in (0..data.len() / 2).rev() {
+        sift_down(data, i, &mut less_than);
     }
 }
 
-impl<T, F: FnMut(&T, &T) -> Ordering> KMergePredicate<T> for F {
-    fn kmerge_pred(&mut self, a: &T, b: &T) -> Ordering {
-        self(a, b)
+/// Sift down element at `index` (`heap` is a min-heap wrt the ordering)
+fn sift_down<T, S>(heap: &mut [T], index: usize, mut less_than: S)
+where
+    S: FnMut(&T, &T) -> bool,
+{
+    debug_assert!(index <= heap.len());
+    let mut pos = index;
+    let mut child = 2 * pos + 1;
+    // Require the right child to be present
+    // This allows to find the index of the smallest child without a branch
+    // that wouldn't be predicted if present
+    while child + 1 < heap.len() {
+        // pick the smaller of the two children
+        // use aritmethic to avoid an unpredictable branch
+        child += less_than(&heap[child + 1], &heap[child]) as usize;
+
+        // sift down is done if we are already in order
+        if !less_than(&heap[child], &heap[pos]) {
+            return;
+        }
+        heap.swap(pos, child);
+        pos = child;
+        child = 2 * pos + 1;
+    }
+    // Check if the last (left) child was an only child
+    // if it is then it has to be compared with the parent
+    if child + 1 == heap.len() && less_than(&heap[child], &heap[pos]) {
+        heap.swap(pos, child);
     }
 }
 
 pin_project! {
-    pub struct KWayMerge<S, F>
+    /// A stream adaptor that merges an abitrary number of base streams
+    /// according to an ordering function.
+    ///
+    /// Iterator element type is `S::Item`.
+    ///
+    /// See [`.kmerge_by()`](crate::kmerge_by) for more
+    /// information.
+    #[must_use = "stream adaptors are lazy and do nothing unless consumed"]
+    pub struct KWayMergeBy<S, F>
     where
         S: Stream,
         S: Unpin,
@@ -45,59 +71,41 @@ pin_project! {
         initial: Option<JoinAll<StreamFuture<S>>>,
         next: Option<S>,
         heap: Vec<HeadTail<S>>,
-        compare: F,
+        less_than: F,
     }
 }
 
-impl<S, F> Stream for KWayMerge<S, F>
-where
-    S: Stream,
-    S: Unpin,
-    F: KMergePredicate<S::Item>,
-{
-    type Item = S::Item;
+/// A stream adaptor that merges an abitrary number of base streams in ascending order.
+/// If all base streams are sorted (ascending), the result is sorted.
+///
+/// Stream element type is `S::Item`.
+///
+/// See [`.kmerge()`](crate::kmerge) for more information.
+#[must_use = "stream adaptors are lazy and do nothing unless consumed"]
+pub type KWayMerge<I> = KWayMergeBy<I, KMergeByLt>;
 
-    fn poll_next(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Self::Item>> {
-        let this = self.project();
-        if let Some(init_fut) = this.initial.as_mut() {
-            let xs = ready!(init_fut.poll_unpin(cx));
-            *this.initial = None;
-            xs.into_iter()
-                .filter_map(|(head_option, tail)| head_option.map(|head| HeadTail { head, tail }))
-                .for_each(|head_tail| this.heap.push(head_tail));
-            this.heap
-                .sort_unstable_by(|x, y| this.compare.kmerge_pred(&y.head, &x.head));
-        }
+pub trait KMergePredicate<T> {
+    fn kmerge_pred(&mut self, a: &T, b: &T) -> bool;
+}
 
-        if let Some(ref mut next_stream) = this.next {
-            if let Some(item) = ready!(next_stream.next().poll_unpin(cx)) {
-                let pos = this
-                    .heap
-                    .binary_search_by(|x| this.compare.kmerge_pred(&item, &x.head))
-                    .unwrap_or_else(|pos| pos);
-                this.heap.insert(
-                    pos,
-                    HeadTail {
-                        head: item,
-                        tail: this.next.take().unwrap(),
-                    },
-                )
-            }
-        }
+#[derive(Clone, Debug)]
+pub struct KMergeByLt;
 
-        match this.heap.pop() {
-            Some(HeadTail { head, tail }) => {
-                this.next.replace(tail);
-                Poll::Ready(Some(head))
-            }
-            None => Poll::Ready(None),
-        }
+impl<T: PartialOrd> KMergePredicate<T> for KMergeByLt {
+    fn kmerge_pred(&mut self, a: &T, b: &T) -> bool {
+        a < b
     }
 }
 
+impl<T, F: FnMut(&T, &T) -> bool> KMergePredicate<T> for F {
+    fn kmerge_pred(&mut self, a: &T, b: &T) -> bool {
+        self(a, b)
+    }
+}
+
+/// Create a stream that merges elements of the contained streams using
+/// the ordering function.
+///
 /// ```
 /// # tokio_test::block_on(async {
 /// use futures::{stream, StreamExt};
@@ -111,25 +119,164 @@ where
 /// );
 /// # })
 /// ```
-pub fn kmerge<S>(xs: impl IntoIterator<Item = S>) -> KWayMerge<S, KMergeByLt>
+pub fn kmerge<S>(xs: impl IntoIterator<Item = S>) -> KWayMerge<S>
 where
     S: Stream + Unpin,
-    S::Item: Ord,
+    S::Item: PartialOrd,
 {
-    kmerge_by(xs, KMergeByLt)
+    assert_stream::<S::Item, _>(kmerge_generic(xs, KMergeByLt))
 }
 
-pub fn kmerge_by<S, F>(xs: impl IntoIterator<Item = S>, compare: F) -> KWayMerge<S, F>
+/// Create a stream that merges elements of the contained streams.
+///
+/// ```
+/// # tokio_test::block_on(async {
+/// use futures::{stream, StreamExt};
+/// use stream_kmerge::kmerge_by;
+///
+/// let streams = vec![stream::iter(vec![5, 3, 1]), stream::iter(vec![4, 3, 2])];
+///
+/// assert_eq!(
+///     kmerge_by(streams, |x, y| y < x).collect::<Vec<usize>>().await,
+///     vec![5, 4, 3, 3, 2, 1],
+/// );
+/// # })
+/// ```
+pub fn kmerge_by<S, F>(
+    xs: impl IntoIterator<Item = S>,
+    less_than: F,
+) -> KWayMergeBy<S, F>
+where
+    S: Stream + Unpin,
+    F: FnMut(&S::Item, &S::Item) -> bool,
+{
+    kmerge_generic(xs, less_than)
+}
+
+/// This was originally meant to be [`kmerge_by`], but triggers a compiler bug, if you directly pass
+/// a closure without type hint for `less_than` (https://github.com/rust-lang/rust/issues/81511).
+/// More specifically, the following code fails to compile:
+///
+/// ```norust
+/// use stream_kmerge::kmerge_generic;
+/// use futures::stream;
+///
+/// kmerge_generic(vec![stream::empty::<usize>()], |x, y| x < y);
+/// ```
+///
+/// If you add a type hint to the closure parameter's, it works:
+///
+/// ```norust
+/// use stream_kmerge::kmerge_generic;
+/// use futures::stream;
+///
+/// kmerge_generic(vec![stream::empty::<usize>()], |x: &_, y: &_| x < y);
+/// ```
+///
+/// The error message is
+/// ```norust
+/// error[E0308]: mismatched types
+///    --> src/lib.rs:165:1
+///     |
+/// 7   | kmerge_generic(vec![stream::empty::<usize>()], |x, y| x < y);
+///     | ^^^^^^^^^^^^^^ lifetime mismatch
+///     |
+///     = note: expected type `for<'r, 's> FnMut<(&'r usize, &'s usize)>`
+///                found type `FnMut<(&usize, &usize)>`
+/// note: this closure does not fulfill the lifetime requirements
+///    --> src/lib.rs:165:48
+///     |
+/// 7   | kmerge_generic(vec![stream::empty::<usize>()], |x, y| x < y);
+///     |                                                ^^^^^^^^^^^^
+/// note: the lifetime requirement is introduced here
+///    --> /home/thomas/src/stream-kmerge/src/lib.rs:171:8
+///     |
+/// 171 |     F: KMergePredicate<S::Item>,
+///     |        ^^^^^^^^^^^^^^^^^^^^^^^^
+///
+/// error: aborting due to previous error
+///
+/// For more information about this error, try `rustc --explain E0308`.
+/// Couldn't compile the test.
+/// ```
+///
+/// Therefore, `kmerge_generic` is private and the public [`kmerge_by`] explicitly takes a closure
+/// instead of [`KMergePredicate`].
+fn kmerge_generic<S, F>(
+    xs: impl IntoIterator<Item = S>,
+    less_than: F,
+) -> KWayMergeBy<S, F>
 where
     S: Stream + Unpin,
     F: KMergePredicate<S::Item>,
 {
-    KWayMerge {
-        initial: Some(join_all(xs.into_iter().map(|x| x.into_future()))),
+    let iter = xs.into_iter();
+    let (min_size, _) = iter.size_hint();
+    assert_stream::<S::Item, _>(KWayMergeBy {
+        initial: Some(join_all(iter.map(|x| x.into_future()))),
         next: None,
-        heap: Vec::new(),
-        compare,
+        heap: Vec::with_capacity(min_size),
+        less_than,
+    })
+}
+
+impl<S, F> Stream for KWayMergeBy<S, F>
+where
+    S: Stream + Unpin,
+    F: KMergePredicate<S::Item>,
+{
+    type Item = S::Item;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        let this = self.project();
+        if let Some(init_fut) = this.initial.as_mut() {
+            let xs = ready!(init_fut.poll_unpin(cx));
+            *this.initial = None;
+            this.heap.extend(
+                xs.into_iter().filter_map(|(head_option, tail)| {
+                    head_option.map(|head| HeadTail { head, tail })
+                }),
+            );
+            heapify(this.heap, |a, b| {
+                this.less_than.kmerge_pred(&a.head, &b.head)
+            });
+        }
+
+        if let Some(ref mut next_stream) = this.next {
+            if let Some(item) = ready!(next_stream.next().poll_unpin(cx)) {
+                this.heap.insert(
+                    0,
+                    HeadTail {
+                        head: item,
+                        tail: this.next.take().unwrap(),
+                    },
+                );
+                sift_down(this.heap, 0, |a, b| {
+                    this.less_than.kmerge_pred(&a.head, &b.head)
+                });
+            }
+        }
+
+        if this.heap.is_empty() {
+            return Poll::Ready(None);
+        }
+
+        let HeadTail { head, tail } = this.heap.remove(0);
+        this.next.replace(tail);
+        Poll::Ready(Some(head))
     }
+}
+
+// Just a helper function to ensure the streams we're returning all have the
+// right implementations.
+fn assert_stream<T, S>(stream: S) -> S
+where
+    S: Stream<Item = T>,
+{
+    stream
 }
 
 #[cfg(test)]
@@ -145,10 +292,10 @@ mod test {
     use tokio::time;
     use tokio_stream::wrappers::IntervalStream;
 
-    use super::kmerge;
+    use super::*;
 
     #[tokio::test]
-    async fn test() {
+    async fn sync() {
         let streams = vec![stream::iter(vec![1, 3, 5]), stream::iter(vec![2, 3, 4])];
 
         assert_eq!(
@@ -158,7 +305,15 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_async() {
+    async fn by() {
+        let streams = vec![stream::iter(vec![5, 3, 1]), stream::iter(vec![4, 3, 2])];
+        let stream = kmerge_by(streams, |x, y| y < x);
+
+        assert_eq!(stream.collect::<Vec<usize>>().await, vec![5, 4, 3, 3, 2, 1],);
+    }
+
+    #[tokio::test]
+    async fn kmerge_async() {
         let streams = vec![
             IntervalStream::new(time::interval(Duration::from_nanos(1))),
             IntervalStream::new(time::interval(Duration::from_nanos(2))),
@@ -170,7 +325,7 @@ mod test {
     }
 
     #[tokio::test]
-    async fn test_concurrent_initialization() {
+    async fn concurrent_initialization() {
         let (tx1, rx1) = oneshot::channel();
         let (tx2, rx2) = oneshot::channel();
 
